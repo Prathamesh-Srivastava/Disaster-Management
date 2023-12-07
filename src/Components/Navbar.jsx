2b6a66367f783f4140f00098c0c945a403f35ab8
Prathamesh-Srivastava <prathameshsrivastava04@gmail.com>
2023-12-07 20:24:18 +0530

    Added Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li><a href="#" className="nav-link">Home</a></li>
+        <li><a href="#" className="nav-link">About</a></li>
+        <li><a href="#" className="nav-link">Services</a></li>
+        <li><a href="#" className="nav-link">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
