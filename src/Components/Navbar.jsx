35a67748e7f2c6af94897750f58ddc345b46325a
AnshZala <202151025@iiitvadodara.ac.in>
2023-12-07 20:54:17 +0530

    modified navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,11 +4,17 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <nav className="navbar">
+      <span className='logo'>
+        <span>Disaster</span>
+        <span>Management</span>
+      </span>
       <ul className="nav-list">
         <li><a href="#" className="nav-link">Home</a></li>
         <li><a href="#" className="nav-link">About</a></li>
-        <li><a href="#" className="nav-link">Services</a></li>
+        <li><a href="#" className="nav-link">Donate</a></li>
+        <li><a href="#" className="nav-link">Volunteer</a></li>
         <li><a href="#" className="nav-link">Contact</a></li>
+        <li><a href="#" className="nav-link">Admin</a></li>
       </ul>
     </nav>
   );
